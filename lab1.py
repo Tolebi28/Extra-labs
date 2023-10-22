564fc8c68c4bcc16a887d9a03246aaef9a82f1dd
Tolebi <timing@MacBook-Pro--Tolebi.local>
2023-10-22 19:49:14 +0600

    lab1

---
+++ lab1.py
@@ -0,0 +1,9 @@
+name = input('Введите ваше имя:')
+surname = input('Введите вашу фамилию:')
+age = input('Введите ваш год рождения:')
+extra = input('Нравится вам курс?')
+
+print('Ваши ответы:')
+print('Вас зовут:', name, surname)
+print('Вам', 2023 - int(age), 'лет')
+print('Нравится вам курс?', extra)
